48f4c199546a2bbe3546b92a5d2f72ba221eb754
thierry-nzeutem <thierry.nzeutem@gmail.com>
2025-06-29 18:32:57 +0200

    Fix Vite development proxy configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,19 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
